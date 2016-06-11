abf5fb29a893281c8c9d89675ad2cec235d7638f
konishi15 <hiro.koni704@gmail.com>
2016-06-12 00:43:56 +0900

    pushing konishi's edit

--- FamitoryForBotch/FamitoryForBotch/ViewController.swift
+++ FamitoryForBotch/FamitoryForBotch/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var myTextField2: UITextField!
+    @IBAction func ChangeText2(sender: AnyObject) {
+    }
+    @IBOutlet weak var myTextField1: UITextField!
+    @IBAction func ChangeText1(sender: AnyObject) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
