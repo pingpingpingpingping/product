a151c49f17a0f2b87791d4bb7729a0a1e47bceb5
Hayato Kihara <kai.21grape@gmail.com>
2016-06-11 16:46:29 +0900

    xcode commit

--- FamitoryForBotch/FamitoryForBotch/Detector.mm
+++ FamitoryForBotch/FamitoryForBotch/Detector.mm
@@ -6,4 +6,10 @@
 //  Copyright © 2016年 ping. All rights reserved.
 //
 
-#include "Detector.hpp"
+#import "FamitoryForBotch-Bridging-Header.h"
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgcodecs/ios.h>
+
+@implementation Detector: NSObject
+
+@end
